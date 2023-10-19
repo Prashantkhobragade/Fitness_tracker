3b578758212a4f8aee04c4bcbd6e25c0ca61f573
prashant <prashantkhobragade011@gmail.com>
2023-10-19 14:18:08 +0530

    Classification model completed

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -331,4 +331,50 @@ plt.grid(False)
 plt.show()
 
 
-#
+#lets try NN model with Selected_Features
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y,
+        ) = learner.feedforward_neural_network(
+            X_train[selected_features],
+            y_train,
+            X_test[selected_features],
+            gridsearch=False,
+        )
+        
+        
+accuracy = accuracy_score(y_test, class_test_y)
+classes = class_test_prob_y.columns
+
+#Confusion Metrix
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+#plot
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
+
+"""
+with Selected_feature , Accuracy Is Good 98.606 % but Model IS Confused Between dead and row.
+"""
